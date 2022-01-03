039bcda972a2c976efd07d3273b8295b7e6acbd7
Forest <767425412@qq.com>
2022-01-03 22:37:30 +0800

    feat: 中间件全局注册

--- 05-gin/main.go
+++ 05-gin/main.go
@@ -7,12 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func middwareOne(c *gin.Context){
+func middwareOne(c *gin.Context) {
 	fmt.Println("middleware one")
 	c.Next()
 	fmt.Println("middleware one end")
 }
-func middwareTwo(c *gin.Context){
+func middwareTwo(c *gin.Context) {
 	fmt.Println("middleware two")
 	c.Next()
 	fmt.Println("middleware two end")
@@ -21,6 +21,9 @@ func middwareTwo(c *gin.Context){
 func main() {
 	r := gin.Default()
 
+	r.Use(middwareOne, middwareTwo)
+
+	// 路由中间件
 	r.GET("/", middwareOne, middwareTwo, func(c *gin.Context) {
 		fmt.Println("request")
 		c.JSON(http.StatusOK, gin.H{
@@ -32,14 +35,20 @@ func main() {
 		})
 	})
 	/**
-		gin中的中间件和koa中中间--洋葱模型差不多
-		打印顺序：
-			middleware one
-			middleware two
-			request
-			middleware two end
-			middleware one end
+	gin中的中间件和koa中中间--洋葱模型差不多
+	打印顺序：
+		middleware one
+		middleware two
+		request
+		middleware two end
+		middleware one end
 	*/
+
+	// 该接口请求后也会打印上面注释内容
+	r.GET("/middleware", func(c *gin.Context) {
+		fmt.Println("middleware request")
+		c.String(http.StatusOK, "middleware")
+	})
 
 	r.Run(":9090")
 }
