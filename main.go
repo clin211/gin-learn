c86694f7a69fb2145b6a4af98de6d31e2cd74beb
Forest <767425412lin@gmail.com>
2022-12-02 21:55:21 +0800

    initial project

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"blog/infrastructure"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(context *gin.Context) {
+		infrastructure.LoadEnv()     //loading env
+		infrastructure.NewDatabase() //new database connection
+		context.JSON(http.StatusOK, gin.H{"data": "Hello world!"})
+	})
+
+	router.Run(":8000")
+}
